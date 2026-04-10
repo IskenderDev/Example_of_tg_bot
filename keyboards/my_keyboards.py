698689e32eb21ff2617b683fcbc0a8a9167f3f06
IskenderDev <iskenderkadyrzhanov@gmail.com]>
2026-04-10 17:27:07 +0600

    delete-init.py

--- keyboards/my_keyboards.py
+++ keyboards/my_keyboards.py
@@ -11,7 +11,8 @@ keyboard = ReplyKeyboardMarkup(
 inline_keyboard = InlineKeyboardMarkup(
     inline_keyboard=[
         [InlineKeyboardButton(text="About", url="https://www.youtube.com/watch?v=dQw4w9WgXcQ&list=RDdQw4w9WgXcQ&start_radio=1")],
-        [InlineKeyboardButton(text="Our contacts", callback_data="contact")],
+        [InlineKeyboardButton(text="Our contacts 1 ", callback_data="contact1")],
+        [InlineKeyboardButton(text="Our contacts 2 ", callback_data="contact2")],
     ]
 )
 
